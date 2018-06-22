0dea818559a487a6fc9997d781ee9d6de2e1e8bd
Phillip Kim <phildkim@gmail.com>
2018-06-21 18:40:09 -0700

    added some code from previous lab

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,61 @@
 'use strict';
+
+import express from 'express';
+const router = express.Router();
+
+import modelFinder from '../middleware/models.js';
+router.param('model', modelFinder);
+
+let sendJSON = (res, data) => {
+  res.status(200);
+  res.json(data);
+};
+
+//GET ONE
+router.get('/api/v1/:model/:id', (req, res, next) => {
+  req.model.findOne(req.params.id)
+    .then(data => {
+      sendJSON(res, data);
+    })
+    .catch(next);
+});
+
+//GET ALL
+router.get('/api/v1/:model', (req, res, next) => {
+  req.model.fetchAll()
+    .then(data => sendJSON(res, data))
+    .catch(next);
+});
+
+//DELETE
+router.delete('/api/v1/:model/:id', (req, res, next) => {
+  req.model.deleteOne(req.params.id)
+    .then(() => {
+      res.statusCode = 204;
+      res.statusMessage = 'OK';
+      res.end();
+    })
+    .catch(next);
+});
+
+//POST
+router.post('/api/v1/:model', (req, res, next) => {
+  let record = new req.model(req.body);
+
+  record.save()
+    .then(data => sendJSON(res, data))
+    .catch(next);
+});
+
+router.put('/api/v1/:model/:id', (req, res, next) => {
+  req.model.putThis(req.params.id, req.body)
+    .then((data) => {
+      sendJSON(res, data);
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,4 +1,40 @@
 'use strict';
 
-import api from '../src/api/api.js';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
+import router from '../src/api/api.js';
+
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/404.js';
+
+let app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(router);
+app.use(notFound);
+app.use(errorHandler);
+
+let server;
+
+module.exports = {
+  start: (port) => {
+    if(! server) {
+      server = app.listen(port, (err) => {
+        if(err) { throw err; }
+        console.log('Server running on', port);
+      });
+    }
+    else {
+      console.log('Server is already up');
+    }
+  },
+  stop: () => {
+    server.close( () => {
+      console.log('Server is closed');
+    });
+  },
+};
